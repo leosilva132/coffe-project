2df86fb458741c335f2c35f30a737f85be93ab0f
Leandro Viana Silva <leandroviana.vs@gmail.com>
2022-11-07 11:52:03 -0300

    fix updated cart

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -18,21 +18,47 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
   const [products, setProducts] = useState<Product[]>([])
   const [cart, setCart] = useState<Product[]>([])
 
-  const addProduct = async (productId: number) => {}
+  const addProduct = async (productId: number) => {
+    try {
+      const updatedCart = [...cart]
+      const productExists = updatedCart.find(
+        (product) => product.id === productId,
+      )
+
+      const stock = await api.get(`/stock/${productId}`)
+      const stockAmount = stock.data.amount
+      const currentAmount = productExists ? productExists.amount : 0
+      const amount = currentAmount + 1
+
+      if (amount > stockAmount) {
+        console.log('Produto fora de estoque')
+        return
+      }
+
+      if (productExists) {
+        productExists.amount = amount
+      } else {
+        const product = await api.get(`/products/${productId}`)
+
+        const newProduct = {
+          ...product.data,
+          amount: 1,
+        }
+        updatedCart.push(newProduct)
+      }
+      setCart(updatedCart)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   async function fetchProducts() {
     const response = await api.get('/products')
     setProducts(response.data)
   }
 
-  async function fetchProductsAmount() {
-    const response = await api.get('/stock')
-    setCart(response.data)
-  }
-
   useEffect(() => {
     fetchProducts()
-    fetchProductsAmount()
   }, [])
 
   return (
